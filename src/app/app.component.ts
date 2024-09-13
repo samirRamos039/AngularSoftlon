aa76581354b07216a0983267351718e4801724de
samirRamos039 <70555870+samirRamos039@users.noreply.github.com>
2024-09-13 15:02:56 -0500

    ndvar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +10,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angularSoftlon';
+  constructor(private router: Router) { }
+
+  login() {
+    this.router.navigate(['/auth/login']);
+  }
 }
